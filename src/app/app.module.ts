41bfae8509294b3e41d891dbcf88bd0478ed6873
ItWarior <Tarasbennet@gmail.com>
2021-09-27 17:48:23 +0300

    add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import {NgModule} from '@angular/core';
+
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
+import { FormComponent } from './form/form.component';
+
 import {CommentsService} from "./services/comments.service";
-import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     CommentsService
